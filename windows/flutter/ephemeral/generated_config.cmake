40af639465017585d90373a3ca30600aae6e6748
gitgudn00b <60489592+gitgudn00b@users.noreply.github.com>
2023-11-15 22:28:32 -0600

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\annad\\Documents\\work\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\annad\\Documents\\school\\csc4330\\project_code\\waste_protector" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\annad\\Documents\\work\\flutter"
+  "PROJECT_DIR=C:\\Users\\annad\\Documents\\school\\csc4330\\project_code\\waste_protector"
+  "FLUTTER_ROOT=C:\\Users\\annad\\Documents\\work\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\annad\\Documents\\school\\csc4330\\project_code\\waste_protector\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\annad\\Documents\\school\\csc4330\\project_code\\waste_protector"
+  "FLUTTER_TARGET=C:\\Users\\annad\\Documents\\school\\csc4330\\project_code\\waste_protector\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz1mYWxzZQ==,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9hNzk0Y2YyNjgxYzZjOWZlN2IyNjBlMGU4NGRlOTYyOThkYzljMThiLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\annad\\Documents\\school\\csc4330\\project_code\\waste_protector\\.dart_tool\\package_config.json"
+)
